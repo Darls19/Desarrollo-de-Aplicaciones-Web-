1f56c0ed40d8f2b6de0dd4db8ef9e729ded227cb
Darla <161660982+Darls19@users.noreply.github.com>
2024-06-18 21:24:28 -0600

    Delete Parcial 2/Actividad 2 directory

--- Parcial 2/Actividad 2/ACTIVIDAD 2/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('generar').addEventListener('click', function() {
-    const nombres1 = ['Lisa', 'Jennie', 'Rosé', 'Jisoo', 'Camila', 'Lucía', 'Renata', 'Paola'];
-    const nombres2 = ['Solis', 'López', 'Ortiz', 'Pérez', 'Olivarez', 'Gamboa', 'Sanchez', 'Vargas'];
-
-    const nombreAleatorio1 = nombres1[Math.floor(Math.random() * nombres1.length)];
-    const nombreAleatorio2 = nombres2[Math.floor(Math.random() * nombres2.length)];
-
-    document.getElementById('nombre').textContent = `El nombre generado es: ${nombreAleatorio1} ${nombreAleatorio2}`;
-});
